6dcdd04ff6ac6ff0ced7c224f382a5033b305b3e
devd254 <devond156@gmail.com>
2023-09-16 11:07:26 -0400

    Map data from DB

--- fireline/map/data.py
+++ fireline/map/data.py
@@ -1,9 +1,17 @@
 from pymongo import MongoClient
+import sklearn
+from sklearn import linear_model
 
 client = MongoClient("mongodb+srv://sabdulb:sabdulb@fireeye.aw4lhpu.mongodb.net/")
 
 db = client.get_database('wildfire_db')
 
 records = db.wildfire_data
+query = {"FIRE_NAME": {"$exists": True}}
+data = records.find(query)
+fire_list = []
+for fire in data:
+    fire_location = {"NAME": fire["FIRE_NAME"], "YEAR": fire["FIRE_YEAR"], "STATE": fire["STATE"], "LONG": fire["LONGITUDE"], "LAT": fire["LATITUDE"]}
+    fire_list.append(fire_location)
 
-print(records.count_documents({}))
+print(fire_list)
