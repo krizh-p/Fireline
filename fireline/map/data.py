c920ff91642497d0538f797d7e3054c6a06a94dc
devd254 <devond156@gmail.com>
2023-09-16 11:39:09 -0400

    Map Data Script

--- fireline/map/data.py
+++ fireline/map/data.py
@@ -3,13 +3,16 @@ import sklearn
 from sklearn import linear_model
 
 client = MongoClient("mongodb+srv://sabdulb:sabdulb@fireeye.aw4lhpu.mongodb.net/")
-
 db = client.get_database('wildfire_db')
-
 records = db.wildfire_data
 query = {"FIRE_NAME": {"$exists": True}}
-data = records.find(query)
-fire_list = []
-for fire in data:
-    fire_location = {"NAME": fire["FIRE_NAME"], "YEAR": fire["FIRE_YEAR"], "STATE": fire["STATE"], "LONG": fire["LONGITUDE"], "LAT": fire["LATITUDE"]}
-    fire_list.append(fire_location)
+
+def map_data():
+    data = records.find(query)
+    fire_list = []
+    for fire in data:
+        fire_location = {"NAME": fire["FIRE_NAME"], "YEAR": fire["FIRE_YEAR"], "STATE": fire["STATE"], "LONG": fire["LONGITUDE"], "LAT": fire["LATITUDE"]}
+        fire_list.append(fire_location)
+
+def main():
+    map_data()
