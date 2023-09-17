1c3e5a4148e13c703f12b6ff92cb37c428ce1501
devd254 <devond156@gmail.com>
2023-09-17 00:39:49 -0400

    Predictions variable implementation

--- fireline/home/views.py
+++ fireline/home/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
 
+from Fireline.fireline.map import data, prediction
+
+#Trained Data Predictions
+predictedPoints = prediction.train(data.prediction_data)
+
 # Create your views here.
 def index(request):
     return render(request, "home/index.html")
--- fireline/map/prediction.py
+++ fireline/map/prediction.py
@@ -1,14 +1,6 @@
 from typing import List
 import pandas as pd
 from sklearn import linear_model
-import matplotlib.pyplot as plot
-import data
-
-def export_prediction(lng: List[float], lat: float) -> List[dict]:
-    export = []
-    for i in range(len(lng)):
-        export.append({"LONG": lng[i], "LAT": lat[i], "NAME": f"User {i+1}"})
-    return export      
 
 
 #Train AI with Linear Regression
@@ -27,27 +19,14 @@ def train(prediction_data: dict[List]):
     #Train AI & Predict
     model.fit(X, Y)
     predictions = model.predict(X)
-    #Visualize
-    plot.scatter(X, Y, label='Data Points')
-    plot.plot(X, predictions, color='red', label='Linear Regression Line')
-    plot.xlabel('X-coordinate')
-    plot.ylabel('Y-coordinate')
-    plot.legend()
-    plot.title('Linear Regression for Coordinates')
-    plot.show()
-
+    
     #Create list of predicted points
-    lngs, lats = []
+    points = []
     num_predicts = 3
     for i in range(-1, -1*num_predicts):
-        lngs.append(X.values[i], predictions[-1])
-    
-    #Export points to Map
-    export_prediction(lngs, lats)
-
-def main():
-    spread = data.prediction_data()
-    prediction = train(spread)
-
-if __name__ == '__main__':
-    main()
+        points.append({
+            "LONG" : X.values[i],
+            "LAT" : predictions[i],
+            "NAME": f"User {i}"
+        })
+    return points
